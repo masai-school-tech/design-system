53b032b7fa11fa3df5832b3890a640360c2299d7
Dhaval Chheda <d.chheda85@gmail.com>
2022-02-23 14:30:06 +0530

    refactor(packages): removed lib folder from git

--- packages/foundation/lib/Fontsize.d.ts
+++
@@ -1,8 +0,0 @@
-declare const _default: Readonly<{
-    xs: "xs";
-    sm: "sm";
-    base: "base";
-    lg: "lg";
-    xl: "xl";
-}>;
-export default _default;
--- packages/foundation/lib/index.js
+++
@@ -1,3 +0,0 @@
-import FontSize from "./Fontsize";
-import Spacing from "./Spacing";
-export { FontSize, Spacing };
