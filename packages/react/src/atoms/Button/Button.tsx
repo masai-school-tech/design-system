64cee0f8f2fe75bb23005125d95e41c0f0f24d0d
Dhaval Chheda <d.chheda85@gmail.com>
2022-02-28 11:04:58 +0530

    style(packages/scss packages/react): demoing some things

--- packages/react/src/atoms/Button/Button.tsx
+++ packages/react/src/atoms/Button/Button.tsx
@@ -4,6 +4,7 @@ interface ButtonProps {
   label: string;
 }
 
+console.log("hello");
 const Button: React.FunctionComponent<ButtonProps> = ({ label }) => {
   return <button className='masai-button__container'>{label}</button>;
 };
